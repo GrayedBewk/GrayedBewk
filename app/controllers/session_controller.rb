55bbe3084a45a6173ba26e257547020b8d305a75
Ryan Graham <ryangrahamwpb@gmail.com>
2015-10-09 13:53:44 -0400

    Add login page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,7 +3,16 @@ class SessionController < ApplicationController
   end
 
   def create
-    teacher = Teacher.create()
+    user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    # Log the user in and redirect to the user's show page.
+  else
+     flash[:danger] = 'Invalid email/password combination' # Not quite right!
+     render 'new'
+  end
+end
+
+  def index
   end
 
   def destroy
