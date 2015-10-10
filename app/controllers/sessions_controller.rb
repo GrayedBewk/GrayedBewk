09f19814e68fd09f2d64a37414cd58140f710dc9
Josh McKelvey <jmckelvey0@gmail.com>
2015-10-10 16:47:05 -0400

    fix a great many routing errors in the views, most are working now

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,82 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  # def create
+  #   render 'new'
+  # end
+
+  def create
+    # Search for user in databases. One of these will fill with information, the other two will be nil.
+    t = Teacher.find_by_email(params[:email])
+    s = Student.find_by_email(params[:email])
+    p = Parent.find_by_email(params[:email])
+    # Check the above variables to see which user type we are dealing with and then populate session variables.
+    if t && t.authenticate(params[:password])
+      session[:login] = true
+      session[:name] = t.name
+      session[:user_type] = t.class.to_s
+      session[:user_id] = t.id
+      redirect_to teachers_path, notice: "You have been successfully logged in."
+    elsif s && s.authenticate(params[:password])
+      session[:login] = true
+      session[:name] = s.name
+      session[:user_type] = s.class.to_s
+      session[:user_id] = s.id
+      redirect_to students_path, notice: "You have been successfully logged in."
+    elsif p && p.authenticate(params[:password])
+      session[:login] = true
+      session[:name] = p.name
+      session[:user_type] = p.class.to_s
+      session[:user_id] = p.id
+      redirect_to parents_path, notice: "You have been successfully logged in."
+    else
+      # If user is not found, redirect to login page.
+      # redirect_to login_path, notice: "Email/password incorrect.  Please try again or create a new account."
+      flash[:notice] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  # def index
+  #   # Search for user in databases. One of these will fill with information, the other two will be nil.
+  #   t = Teacher.find_by(email: params[:session][:email])
+  #   s = Student.find_by(email: params[:session][:email])
+  #   p = Parent.find_by(email: params[:session][:email])
+  #   # Check the above variables to see which user type we are dealing with and then populate session variables.
+  #   if t && t.authenticate(params[:session][:password])
+  #     session[:login] = true
+  #     session[:name] = t.name
+  #     session[:user_type] = t.class.to_s
+  #     session[:teacher_id] = t.id
+  #     redirect_to root_path, notice: "You have been successfully logged in."
+  #   elsif s && s.authenticate(params[:session][:password])
+  #     session[:login] = true
+  #     session[:name] = s.name
+  #     session[:user_type] = s.class.to_s
+  #     session[:student_id] = t.id
+  #     redirect_to root_path, notice: "You have been successfully logged in."
+  #   elsif p && p.authenticate(params[:session][:password])
+  #     session[:login] = true
+  #     session[:name] = p.name
+  #     session[:user_type] = p.class.to_s
+  #     session[:parent_id] = t.id
+  #     redirect_to root_path, notice: "You have been successfully logged in."
+  #   else
+  #     # If user is not found, redirect to login page.
+  #     redirect_to login_path, notice: "Email/password incorrect.  Please try again or create a new account."
+  #   end
+  # end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  private
+
+    def set_params
+      params.require(:session).permit(:name, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  root   'session#index'                # Dashboard view that displays information based on user type
-  get    'login' => 'session#new'       # Login screen for logged out or new users.  Root redirects here if user is not logged in.
-  post   'login' => 'session#create'    # Create new user screen.  Only accessible from teacher dashboard.  From login screen a user can only choose parent or student.
-  delete 'logout' => 'session#destroy'  # Action to logout and present login screen.
+  root   'sessions#index'                # Dashboard view that displays information based on user type
+  get    'login' => 'sessions#new'       # Login screen for logged out or new users.  Root redirects here if user is not logged in.
+  post   'login' => 'sessions#create'    # Create new user screen.  Only accessible from teacher dashboard.  From login screen a user can only choose parent or student.
+  delete 'logout' => 'sessions#destroy'  # Action to logout and present login screen.
 
   resources :grades
   resources :parents
   resources :students
   resources :teachers
-  resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
